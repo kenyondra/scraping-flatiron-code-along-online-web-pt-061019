e4c92bf6d00d43d96f3bdf8507bc48a39cfb49a8
Kenyondra J Conley <kenyondra_conley@yahoo.com>
2019-07-31 04:58:42 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,8 +9,8 @@ class Scraper
     
     Nokogirl::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
     
-    #doc.css(".post").each do |post|
-      #course = Course.new 
+    doc.css(".post").each do |post|
+      course = Course.new 
       #course.title = post.css("h2").text 
       #course.schedule = post.css(".date").text 
       #course.description = post.css("p").text 
@@ -32,7 +32,6 @@ end
       course.description = post.css("p").text 
     end
   end
-  
   
     def print_courses
     self.make_courses
