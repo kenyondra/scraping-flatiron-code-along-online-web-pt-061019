3234327f67d9105d05b8f159c71fde5d0f8cea68
Kenyondra J Conley <kenyondra_conley@yahoo.com>
2019-07-31 04:35:08 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -19,6 +19,9 @@ class Scraper
       course.title = post.css("h2").text 
       course.schedule = post.css(".date").text 
       course.description = post.css("p").text 
+    end
+  end
+  
   
     def print_courses
     self.make_courses
