6d7b020d485aff6a9ba4bcb3f9b01d86fc64837a
Kenyondra J Conley <kenyondra_conley@yahoo.com>
2019-07-31 04:28:53 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,7 +6,7 @@ require_relative './course.rb'
 class Scraper
   
   def get_page
-    doc = Nokogirl::HTML(open(http://learn-co-curriculum.github.io/site)) 
+    doc = Nokogirl::HTML(open(http://learn-co-curriculum.github.io/site-for-scraping)) 
   
     def print_courses
     self.make_courses
